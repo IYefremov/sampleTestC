523e7bc710513e422ff89c2cbe2b35f522b50f2f
IvanYefremov <iyefremov@yahoo.com>
2020-03-05 21:58:08 +0200

    MyFirstUpdate

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -17,8 +17,29 @@ public class Calculator {
         return a * b;
     }
 
+    public double division(int a, int b) {
+
+            return (double) a / b;
+
+    }
+    public int substraction(int a, int b) {
+
+        return a - b;
+    }
+
+    public int squareRoot(int a) {
+
+        return (int) Math.sqrt(a) ;
+    }
+
+    public int xX (int a){
+        return a * a;
+    }
+
     //TODO
     //add methods for multiply, division, subtraction, square root, x^2
+    //division by 0
+    // SQRT from interrogative number
 
 }
 
--- src/test/java/AddTwoValuesTests.java
+++ src/test/java/AddTwoValuesTests.java
@@ -28,8 +28,65 @@ public class AddTwoValuesTests {
         assertEquals("-10 + -5 must be -15", -15, calc.add(-10, -5));
     }
 
+
     //TODO
     //add new tests for add() method
+    @Test
+    public void testAddTwoValues() {
+        int first = 10;
+        int second = 5;
+        int expected = 15;
+        assertEquals(String.format("%s  + %s = %s",  first, second, expected), expected, calc.add(first, second));
+    }
+
+    //public int multiply(int a, int b)
+    @Test
+    public void testMultiply() {
+        int first = 10;
+        int second = 5;
+        int expected = 50;
+        assertEquals(String.format("%s * %s = %s",  first, second, expected), expected, calc.multiply(first, second));
+    }
+   // public int division(int a, int b) {
+   @Test
+    public void testDivision() {
+        int first = -10;
+        int second = 5;
+        double expected = -2;
+        assertEquals(String.format("%s / %s = %s",  first, second, expected), expected, calc.division(first, second));
+    }
+
+    @Test
+    public void testDivision2() {
+        int first = 9;
+        int second = 2;
+        double expected = 4.5;
+        assertEquals(String.format("%s / %s = %s",  first, second, expected), expected, calc.division(first, second));
+    }
+   // public int substraction(int a, int b) {
+   @Test
+   public void testSubstriction() {
+       int first = 10;
+       int second = 5;
+       int expected = 5;
+       assertEquals(String.format("%s - %s = %s",  first, second, expected), expected, calc.substraction(first, second));
+   }
+   // public int squareRoot(int a) {
+   @Test
+   public void testSquareRoot() {
+       int first = 25;
+       double expected = 5;
+       assertEquals(String.format("SQRT %s = %s", first, expected), expected, calc.squareRoot(first));
+   }
+   // public int xX (int a){
+   @Test
+   public void testXX() {
+       int first = 5;
+       int expected = 25;
+       assertEquals(String.format("x^2 %s = %s", first, expected), expected, calc.xX(first));
+   }
+
+
 
     //TODO
     //add tests for new methods
